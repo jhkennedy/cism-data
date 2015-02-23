907b2f1b14336cadef7e3158726adead04d341f7
Joseph H Kennedy <kennedyjh@ornl.gov>
2015-02-23 18:10:35 -0500

    Complete creation of 1km dataset. Now coarsen. Also remove duplicate flie

--- build_greenland_datasets.py
+++ build_greenland_datasets.py
@@ -29,17 +29,22 @@ if not ( os.path.exists('data/InSAR/Joughin2012/greenland_vel_mosaic500.nc') ):
     subprocess.call("python util/convert_velocities.py", shell=True)
 nc_insar    = Dataset( 'data/InSAR/Joughin2012/greenland_vel_mosaic500.nc' , 'r')
 
+nc_massCon  = Dataset('data/IceBridge/Greenland/MCdataset-2014-11-19.nc','r')
+nc_mask     = Dataset( 'data/Ice2Sea/greenland_geometry_icesheet_mask_Zurich.nc' , 'r'  )
+
+
 #==== Projections ====
 # All the projections 
 # needed for the data 
 #=====================
+#NOTE: Bamber projection appears to not actually have any fasle northings or eastings. 
 proj_latlong  = pyproj.Proj(proj='latlong', datum='WGS84') 
     # needed to convert between projections
-proj_bamber   = pyproj.Proj('+proj=stere +lat_ts=71.0 +lat_0=90 +lon_0=-39.0 +k_0=1.0 +x_0=800000.0 +y_0=3400000.0 +ellps=WGS84 +units=m') 
+#proj_bamber   = pyproj.Proj('+proj=stere +lat_ts=71.0 +lat_0=90 +lon_0=-39.0 +k_0=1.0 +x_0=800000.0 +y_0=3400000.0 +ellps=WGS84 +units=m') 
+proj_bamber   = pyproj.Proj('+proj=stere +lat_ts=71.0 +lat_0=90 +lon_0=-39.0 +k_0=1.0 +ellps=WGS84 +units=m') 
     # basic bamber projection
 proj_epsg3413 = pyproj.Proj('+proj=stere +lat_ts=70.0 +lat_0=90 +lon_0=-45.0 +k_0=1.0 +x_0=0.0 +y_0=0.0 +ellps=WGS84 +units=m')
     # InSAR data in this projections
-proj_wat      = pyproj.Proj('+proj=stere +lat_ts=71.0 +lat_0=90 +lon_0=-39.0 +k_0=1.0 +ellps=WGS84 +units=m') 
 
 # EIGEN-GL04C referenced data
 #----------------------------
@@ -47,7 +52,8 @@ proj_wat      = pyproj.Proj('+proj=stere +lat_ts=71.0 +lat_0=90 +lon_0=-39.0 +k_
 # (and within 10-20 cm in most places) so we use the egm08 projection which is available in proj4
 if not ( os.path.exists('data/BamberDEM/egm08_25.gtx') ):
     raise Exception("No data/BamberDEM/egm08_25.gtx ! Get it here: http://download.osgeo.org/proj/vdatum/egm08_25/egm08_25.gtx") 
-proj_eigen_gl04c = pyproj.Proj('+proj=stere +lat_ts=71.0 +lat_0=90 +lon_0=321.0 +k_0=1.0 +x_0=800000.0 +y_0=3400000.0 +geoidgrids=./data/BamberDEM/egm08_25.gtx')
+#proj_eigen_gl04c = pyproj.Proj('+proj=stere +lat_ts=71.0 +lat_0=90 +lon_0=321.0 +k_0=1.0 +x_0=800000.0 +y_0=3400000.0 +geoidgrids=./data/BamberDEM/egm08_25.gtx')
+proj_eigen_gl04c = pyproj.Proj('+proj=stere +lat_ts=71.0 +lat_0=90 +lon_0=321.0 +k_0=1.0 +geoidgrids=./data/BamberDEM/egm08_25.gtx')
 
 #===== Bamber DEM =====
 # this is a 1km dataset
@@ -139,17 +145,10 @@ insar_ny = insar_y[:].shape[0]
 insar_x = nc_insar.variables['x']
 insar_nx = insar_x[:].shape[0]
 
-#insar_vy = nc_insar.variables['vy']
-#insar_ey = nc_insar.variables['ey']
-
-#insar_vx = nc_insar.variables['vx']
-#insar_ex = nc_insar.variables['ex']
-
-
 # transform meshes
 insar_y_grid, insar_x_grid = scipy.meshgrid( insar_y[:], insar_x[:] , indexing='ij')
 
-trans_x_grid, trans_y_grid = pyproj.transform(proj_epsg3413, proj_wat, insar_x_grid.flatten(), insar_y_grid.flatten())
+trans_x_grid, trans_y_grid = pyproj.transform(proj_epsg3413, proj_eigen_gl04c, insar_x_grid.flatten(), insar_y_grid.flatten())
 trans_y_grid = trans_y_grid.reshape((insar_ny,insar_nx))
 trans_x_grid = trans_x_grid.reshape((insar_ny,insar_nx))
 
@@ -159,8 +158,8 @@ trans_x = trans_x_grid[0,:]
 insar_data = np.ndarray( (insar_ny, insar_nx) )
 base_data = np.ndarray( (base_ny,base_nx) )
 
-var_list = ['vy','vx','ey','ex']
-for vv in var_list :
+
+for vv in ['vy','vx','ey','ex'] :
     insar_data[:,:] = 0.
     base_data[:,:] = 0.
     
@@ -176,14 +175,169 @@ for vv in var_list :
     base_var[:,:] = base_data[:,:]
     copy_atts(insar_var, base_var)
 
+# drop temp. variables
+trans_y_grid = None
+trans_x_grid = None
+trans_y = None
+trans_x = None
 nc_insar.close()
 #==== Mass Conserving Bed Data ===
 # This is the new (2015) bed data 
 #=================================
+# new vars: 'thk' 'topg' 'topgerr' 'usrf'** make from thk+topg?? 'usrfRMSE'** doesn't actually exist.
+massCon_y = nc_massCon.variables['y']
+massCon_ny = massCon_y[:].shape[0]
+
+massCon_x = nc_massCon.variables['x']
+massCon_nx = massCon_x[:].shape[0]
+
+# transform meshes
+massCon_y_grid, massCon_x_grid = scipy.meshgrid(massCon_y[::-1], massCon_x[:], indexing='ij')  # y fliped when compaired to Bamber
+
+#massCon_bed       = nc_massCon.variables['bed']
+#massCon_thickness = nc_massCon.variables['thickness']
+#massCon_surface   = nc_massCon.variables['surface']
+#massCon_errbed    = nc_massCon.variables['errbed']
+#massCon_mask      = mc_massCon.variables['mask']
+
+massCon_data = np.ndarray( (massCon_ny,massCon_nx) )
+trans_data   = np.ndarray( (massCon_ny,massCon_nx) )
+base_data    = np.ndarray( (base_ny,base_nx) )
+
+var_list       = [ 'thickness', 'bed' ]
+rename_massCon = [ 'thk',       'topg']
+for vv in range(0, len(var_list) ) :
+    var = var_list[vv]
+    massCon_data[:,:] = 0.
+    trans_data[:,:]   = 0.
+    base_data[:,:]    = 0.
+
+    massCon_var = nc_massCon.variables[var]
+    massCon_data[:,:] = massCon_var[::-1,:] # y fliped when compaired to Bamber
+
+    trans_x_grid, trans_y_grid, trans_data = pyproj.transform(proj_epsg3413, proj_eigen_gl04c, massCon_x_grid.flatten(), massCon_y_grid.flatten(), massCon_data.flatten())
+    trans_data   =   trans_data.reshape( (massCon_ny,massCon_nx) )
+    trans_y_grid = trans_y_grid.reshape( (massCon_ny,massCon_nx) ) 
+    trans_x_grid = trans_x_grid.reshape( (massCon_ny,massCon_nx) ) 
+
+    trans_y = trans_y_grid[:,0]
+    trans_x = trans_x_grid[0,:]
+    
+
+    massCon_to_base = interpolate.RectBivariateSpline( trans_y[:], trans_x[:], trans_data, s=0) # regular 2d linear interp. but faster
+
+    for ii in range(0, base_nx):
+        base_data[:,ii] = massCon_to_base.ev(base_y_grid[:,ii], base_x_grid[:,ii] )
+    
+    
+    base_var = nc_base.createVariable( rename_massCon[vv], 'f4', ('y','x',) )
+    base_var[:,:] = base_data[:,:]
+    #copy_atts(massCon_var, base_var)
+    #NOTE: this data is formated short, while the rest of the data is a float. Does not like _FillValue from short data. 
+    atts = massCon_var.ncattrs()
+    for ii in range(len(atts)):
+        if (atts[ii] != '_FillValue'):
+            base_var.setncattr(atts[ii], massCon_var.getncattr(atts[ii]))
+        #else:
+        #    base_var.setncattr('_FillValue', -9999.)
+
+# Now get bed error -- not in for loop because not z data!
+#---------------------------------------------------------
+base_data[:,:]    = 0.
+
+massCon_errbed = nc_massCon.variables['errbed']
+
+#trans_x_grid, trans_y_grid = pyproj.transform(proj_epsg3413, proj_eigen_gl04c, massCon_x_grid.flatten(), massCon_y_grid.flatten())
+#trans_y_grid = trans_y_grid.reshape( (massCon_ny,massCon_nx) ) 
+#trans_x_grid = trans_x_grid.reshape( (massCon_ny,massCon_nx) ) 
+#
+#trans_y = trans_y_grid[:,0]
+#trans_x = trans_x_grid[0,:]
+
+massCon_to_base = interpolate.RectBivariateSpline( trans_y[:], trans_x[:], massCon_errbed[::-1,:], s=0) # regular 2d linear interp. but faster
+                                                                            # y fliped when compaired to Bamber
+for ii in range(0, base_nx):
+    base_data[:,ii] = massCon_to_base.ev(base_y_grid[:,ii], base_x_grid[:,ii] )
+
+base_var = nc_base.createVariable( 'topgerr', 'f4', ('y','x',) )
+base_var[:,:] = base_data[:,:]
+#copy_atts(massCon_errbed, base_var)
+#NOTE: this data is formated short, while the rest of the data is a float. Does not like _FillValue from short data. 
+atts = massCon_var.ncattrs()
+for ii in range(len(atts)):
+    if (atts[ii] != '_FillValue'):
+        base_var.setncattr(atts[ii], massCon_var.getncattr(atts[ii]))
+   # else:
+   #     base_var.setncattr('_FillValue', -9999.)
+
+# drop temp. variables
+trans_data = None
+trans_y_grid = None
+trans_x_grid = None
+trans_y = None
+trans_x = None
+nc_massCon.close()
 
 
-
-
-
-
+#==== Zurich mask ====
+# apply mask, and get 
+# new surface variable
+#=====================
 nc_base.close()
+
+nc_base = Dataset('greenland_1km.mcb.nc','r+')
+base_thk  = nc_base.variables['thk']
+thk_data = base_thk[:,:]
+
+base_topg = nc_base.variables['topg']
+topg_data = base_topg[:,:]
+
+mask = nc_mask.variables['IceSheetMask']
+
+thk_data = thk_data * mask[:,:]
+base_thk[:,:] = thk_data
+
+base_usrf = nc_base.createVariable('usrf', 'f4',('y','x',))
+base_usrf[:,:] = thk_data + topg_data
+
+nc_mask.close()
+nc_base.close()
+#==== add time dim ====
+# apply to all the vars
+#======================
+nc_base = Dataset('greenland_1km.mcb.nc','r')
+base_y = nc_base.variables['y']
+base_ny = base_y[:].shape[0]
+base_x = nc_base.variables['x']
+base_nx = base_x[:].shape[0]
+
+
+nc_1km = Dataset('greenland_1km_'+stamp+'.mcb.nc', 'w')
+nc_1km.createDimension('time', None )
+nc_1km.createDimension('y1', base_ny)
+nc_1km.createDimension('x1', base_nx)
+
+time = nc_1km.createVariable('time', 'f4', ('time',))
+y1   = nc_1km.createVariable('y1',   'f4', ('y1',)  )
+x1   = nc_1km.createVariable('x1',   'f4', ('x1',)  )
+
+copy_atts(base_y, y1)
+copy_atts(base_x, x1)
+
+y1[:] = base_y[:]
+x1[:] = base_x[:]
+time[0] = 0.
+
+for var_name, var_data in nc_base.variables.iteritems() : 
+    if (var_name != 'x' and var_name != 'y'):
+        var_1km = nc_1km.createVariable(var_name, 'f4', ('time','y1','x1',))
+        var_1km[0,:,:] = var_data[:,:]
+        copy_atts(var_data, var_1km)
+#copy_atts(var_data, var_1km)
+
+nc_1km.close()
+nc_base.close()
+#==== Stamp and coarsen ====
+# make 1, 2, 4 and 8 km data
+#===========================
+
